6e4014e74ff96de3c71035eb97d4b3895babe82f
Johannes Dach <jdach@c3a8c4.42berlin.de>
2024-07-17 18:40:39 +0200

    multi pipes

--- src/exe/exe_get_in_out.c
+++ src/exe/exe_get_in_out.c
@@ -6,25 +6,11 @@
 /*   By: jdach <jdach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 11:17:44 by jdach             #+#    #+#             */
-/*   Updated: 2024/07/17 18:03:48 by jdach            ###   ########.fr       */
+/*   Updated: 2024/07/17 18:21:20 by jdach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	exe_get_out(t_cmd_list *cmd_list_item, t_cmd *cmd_env)
-{
-	if (cmd_list_item->next && cmd_list_item->next->type == T_PIPE)
-	{
-		if (cmd_env->saved_pipe_read_fd != -1)
-			cmd_env->saved_pipe_read_fd = cmd_env->pipe[0];
-		pipe(cmd_env->pipe);
-		dup2(cmd_env->pipe[1], STDOUT_FILENO);
-		close(cmd_env->pipe[1]);
-	}
-	else
-		dup2(cmd_env->saved_stdout, STDOUT_FILENO);
-}
 
 void	exe_get_in(t_cmd_list *cmd_list_item, t_cmd *cmd_env)
 {
@@ -37,8 +23,20 @@ void	exe_get_in(t_cmd_list *cmd_list_item, t_cmd *cmd_env)
 		dup2(cmd_env->saved_stdin, STDIN_FILENO);
 }
 
+void	exe_get_out(t_cmd_list *cmd_list_item, t_cmd *cmd_env)
+{
+	if (cmd_list_item->next && cmd_list_item->next->type == T_PIPE)
+	{
+		pipe(cmd_env->pipe);
+		dup2(cmd_env->pipe[1], STDOUT_FILENO);
+		close(cmd_env->pipe[1]);
+	}
+	else
+		dup2(cmd_env->saved_stdout, STDOUT_FILENO);
+}
+
 void	exe_get_in_out(t_cmd_list *cmd_list_item, t_cmd *cmd_env)
 {
-	exe_get_out(cmd_list_item, cmd_env);
 	exe_get_in(cmd_list_item, cmd_env);
+	exe_get_out(cmd_list_item, cmd_env);
 }
