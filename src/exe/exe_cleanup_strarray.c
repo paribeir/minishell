c7c4ea68be141f0bc1f7b3d374cb092ae17745f9
Johannes Dach <johannesdach@gmail.com>
2024-08-19 16:37:32 +0200

    one segfault less

--- src/exe/exe_cleanup_strarray.c
+++ src/exe/exe_cleanup_strarray.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exe_clenaup_strarray.c                             :+:      :+:    :+:   */
+/*   exe_cleanup_strarray.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdach <jdach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 22:18:28 by jdach             #+#    #+#             */
-/*   Updated: 2024/07/29 22:20:10 by jdach            ###   ########.fr       */
+/*   Updated: 2024/08/19 16:36:14 by jdach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ void	exe_cleanup_strarray(char **strarray)
 	int	i;
 
 	i = -1;
-	while (strarray[++i] != NULL)
+	if (strarray)
+	{
+		while (strarray[++i] != NULL)
+			free (strarray[i]);
 		free (strarray[i]);
-	free (strarray[i]);
-	free (strarray);
+		free (strarray);
+	}
 }
