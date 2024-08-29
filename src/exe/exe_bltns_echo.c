87eeed3de7e5b4565de4706c4b42abe229ba388a
johdac <42833078+johdac@users.noreply.github.com>
2024-08-29 22:09:17 +0000

    improved echo

--- src/exe/exe_bltns_echo.c
+++ src/exe/exe_bltns_echo.c
@@ -6,7 +6,7 @@
 /*   By: jdach <jdach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 18:02:37 by jdach             #+#    #+#             */
-/*   Updated: 2024/08/29 19:30:17 by jdach            ###   ########.fr       */
+/*   Updated: 2024/08/29 22:07:24 by jdach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	exe_bltns_echo_set_n_flag(char **args, int *n_flag, int *start)
 {
 	int	i;
 
-	i = 2;
+	i = 1;
 	if (*args)
 	{
 		if (args[0][0] == '-' && args[0][1] == 'n')
 		{
-			while (args[0][i++] == 'n')
-				;
+			while (args[0][++i] == 'n')
+				continue ;
 			if (args[0][i] == '\0')
 				*n_flag = 1;
 			else
@@ -44,7 +44,7 @@ void	exe_bltns_echo(t_cmd_list *cmd_list_item, t_cmd *cmd_data)
 	n_flag = 0;
 	start = 1;
 	exe_bltns_echo_set_n_flag(args, &n_flag, &start);
-	while (args[start])
+	while (args[0] && args[start])
 	{
 		if ((n_flag == 0 && start > 0) || (n_flag == 1 && start > 1))
 			ft_putchar_fd(' ', 1);
@@ -52,5 +52,4 @@ void	exe_bltns_echo(t_cmd_list *cmd_list_item, t_cmd *cmd_data)
 	}
 	if (n_flag == 0)
 		ft_putchar_fd('\n', 1);
-	g_status = 0;
 }
