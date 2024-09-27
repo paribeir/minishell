46889851670fe39ffb7ec56d9360e75b00384e23
Johannes Dach <johannesdach@gmail.com>
2024-09-27 10:26:37 +0200

    fixed env variables lookup

--- src/exe/exe_env_get_var_address.c
+++ src/exe/exe_env_get_var_address.c
@@ -6,7 +6,7 @@
 /*   By: jdach <jdach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:13:53 by jdach             #+#    #+#             */
-/*   Updated: 2024/08/04 10:07:42 by jdach            ###   ########.fr       */
+/*   Updated: 2024/09/27 10:23:41 by jdach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 char	*exe_env_get_var_address(char *var, t_cmd *cmd_data)
 {
 	int		i;
+	char	**env_var;
 
 	i = 0;
 	while (cmd_data->envp[i] != NULL)
 	{
-		if (ft_strncmp(cmd_data->envp[i], var, ft_strlen(var)) == 0)
+		env_var = ft_split(cmd_data->envp[i], '=');
+		if (ft_strncmp(var, env_var[0], ft_strlen(var) + 1) == 0)
+		{
+			exe_cleanup_strarray(env_var);
 			return (cmd_data->envp[i]);
+		}
 		i++;
 	}
 	return (NULL);
