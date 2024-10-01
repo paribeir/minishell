26f93b31066d496aff395ce6d24cae6021a67543
patriciaserra <patriciaserra97@gmail.com>
2024-10-01 23:14:27 +0200

    done

--- src/parsing/heredoc_signals.c
+++ src/parsing/heredoc_signals.c
@@ -6,7 +6,7 @@
 /*   By: paribeir <paribeir@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 19:18:11 by paribeir          #+#    #+#             */
-/*   Updated: 2024/10/01 22:03:15 by paribeir         ###   ########.fr       */
+/*   Updated: 2024/10/01 23:13:49 by paribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ void	heredoc_signals_handler(int signum)
 {
 	g_signum = signum;
 	rl_done = 1;
-	rl_replace_line("", 0);
-	rl_on_new_line();
+	//rl_replace_line("", 0);
+	//rl_on_new_line();
 	write(STDERR_FILENO, "\n", 1);
-	rl_redisplay();
 }
 
 void	heredoc_signals_set(void)
